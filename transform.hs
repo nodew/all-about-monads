4d711895a2049f6223ec296082afb65112e4bbc5
Joe Wang <wangqiao11@hotmail.com>
2017-08-31 17:27:30 +0800

    fixed: transform.hs type errors

--- transform.hs
+++ transform.hs
@@ -2,23 +2,35 @@ module Main where
 
 import Text.Pandoc
 
-transformInline (Code _ code) =
-  RawInline "mediawiki" $ "<code>" ++ code ++ "</code>"
+transformInline (Code _ code)
+  = RawInline (Format "mediawiki") $ "<code>" ++ code ++ "</code>"
+
 transformInline x = x
 
-transformBlock (CodeBlock _ code) =
-  RawBlock "mediawiki" $ "<haskell>\n" ++ code ++ "\n</haskell>"
-transformBlock (Table [] [AlignLeft, AlignLeft, AlignLeft] [0.0, 0.0, 0.0] [] _) =
-  Null
-transformBlock (BulletList ([Plain [Link _ ('#':_, "")]:_)) = Null
-transformBlock HorizontalRule = Null
+transformBlock (CodeBlock _ code)
+  = RawBlock (Format "mediawiki") $ "<haskell>\n" ++ code ++ "\n</haskell>"
+
+transformBlock (Table [] [AlignLeft,AlignLeft,AlignLeft] [0.0,0.0,0.0] [] _)
+  = Null
+
+transformBlock (BulletList ([Plain [Link _ [] ('#':_,"")]]:_))
+  = Null
+
+transformBlock HorizontalRule
+  = Null
+
 transformBlock x = x
 
-removeNull (Pandoc meta blocks) = Pandoc meta $ filter (/= Null) blocks
+removeNull (Pandoc meta blocks)
+  = Pandoc meta $ filter (/= Null) blocks
 
-main =
-  interact $ writeNative defaultWriterOptions
-           . removeNull
-           . bottomUp transformInline
-           . bottomUp transformBlock
-           . readNative
+readS s = case readNative s of
+            Right doc -> doc
+            Left error -> Pandoc nullMeta []
+
+main :: IO ()
+main = interact $ writeNative (def WriterOptions)
+                . removeNull
+                . bottomUp transformInline
+                . bottomUp transformBlock
+                . readS
